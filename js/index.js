6b0f998c3d541f1725d8a9db79f78a2dfddfd3f4
nikhil upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2022-04-30 14:15:15 +0530

    added logic for food element

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
 //GAME CONSTANTS
 let direction = {x:0, y:0};
- const foodsound = new Audio('food.mp3');
- const musicSound= new Audio('music.mp3');
- const gameOverSound= new Audio('gameover.mp3');
- const moveSound= new Audio('move.mp3');
+const foodsound = new Audio('food.mp3');
+const musicSound= new Audio('music.mp3');
+const gameOverSound= new Audio('gameover.mp3');
+const moveSound= new Audio('move.mp3');
 
- let speed=0;
- let lastPaintTime = 0;
- let snakeArr=[
+let speed=2;
+let lastPaintTime = 0;
+let snakeArr=[
      {x:13 , y:15}
- ]
+]
+food = {x:13 , y:15};
+
 
  //GAME FUNCTIONS
 
  function main(ctime){
      window.requestAnimationFrame(main);
-     console.log(ctime);
+     console.log(ctime)
      if((ctime - lastPaintTime)/1000 < 1/speed){
          return;
      } 
@@ -28,15 +30,23 @@ function gameEngine(){
     //Part 1: Updating the snake array
 
     //PART 2: Display the snake array and food.
-
+           //1.Display the snake
     board.innerHTML = "";
-    snakeArr.forEach((element,index)=>{
+    snakeArr.forEach((e,index)=>{
         snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = element.y;
-        snakeElement.style.gridColumnStart = element.x;
-        snakeElement.classList.add('food');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        snakeElement.classList.add('food')
         board.appendChild(snakeElement);
-    })
+    });
+
+         //2.Display the food
+         foodElement = document.createElement('div');
+         foodlement.style.gridRowStart = food.y;
+        foodElement.style.gridColumnStart = food.x;
+        foodElement.classList.add('food')
+         board.appendChild(foodElement);
+
 }
 
 
