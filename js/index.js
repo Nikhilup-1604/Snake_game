7e24067e6b45493c502f82ea82daabdc034cb7d8
nikhil upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2022-05-01 15:22:39 +0530

    Adding property score and highscore.

--- js/index.js
+++ js/index.js
@@ -62,6 +62,8 @@ function gameEngine(){
 
     if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
         // foodSound.play();
+        score += 1;
+        scoreBox.innerHTML = "Score:" + score;
         //add new body to snake
 
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x , y: snakeArr[0].y + inputDir.y});
