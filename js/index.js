c153d7b3681e3a538575019cc48b9c9137a2782e
nikhil upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2022-05-01 09:48:47 +0530

    today 1-5-22, implementing the isCollide function

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //GAME CONSTANTS
 let inputDir = {x:0, y:0};
-const foodsound = new Audio('food.mp3');
-const musicSound= new Audio('music.mp3');
-const gameOverSound= new Audio('gameover.mp3');
-const moveSound= new Audio('move.mp3');
+// const foodsound = new Audio('food.mp3');
+// const musicSound= new Audio('music.mp3');
+// const gameOverSound= new Audio('gameover.mp3');
+// const moveSound= new Audio('move.mp3');
 
 let score=0;
 let speed=2;
@@ -28,37 +28,54 @@ food = {x:6 , y:7};
 }
 
 function isCollide(sarr){
-    return false;
+    // There are two conditions when snake is collide
+       //1.If it collides to the wall.
+       //2.If it collides with itself.
 }
 
 function gameEngine(){
     //Part 1: Updating the snake array
 
     if(isCollide(snakeArr)){
-        gameOverSound.play();
-        musicSound.pause();
+        // gameOverSound.play();
+        // musicSound.pause();
 
         inputDir = {x:0 , y:0};
         alert("GAME OVER!! Press any key to play again");
         snakeArr = [{x:13 , y:15}];
-        musicSound.play();
+        // musicSound.play();
         score= 0;
     }
 
     //if you have eaten the food, increment the score and regenerate the food
 
     if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
-
+        // foodSound.play();
         //add new body to snake
 
-        snakeArr.unshift({x: snakeArr[0].x + inputDir.x , y: snakeArr[0].y + inputDir.y})
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x , y: snakeArr[0].y + inputDir.y});
 
         //add new food
         let a = 2;
         let b = 16; 
-        food = {x: Math.round(a + (b - a) * Math.random())} //generates random number between a an b ( 2 to 16)
+        food = {x: Math.round(a + (b - a) * Math.random())
+           , y: Math.round(a + (b - a) * Math.random())
+        } //generates random number between a an b ( 2 to 16)
 
     }
+
+
+    //Moving the snake
+
+    for (let i = snakeArr.length - 2; i >=0; i--) {
+        
+        snakeArr[i+1] = {...snakeArr[i]}; 
+        // snakeArr[0]  wale lo kahan le jana hain bs isme dikkat  hain 
+    }
+
+        snakeArr[0].x += inputDir.x;
+        snakeArr[0].y += inputDir.y;
+        
 
 
 
@@ -96,25 +113,25 @@ function gameEngine(){
 
  window.addEventListener('keydown',e=>{
     inputDir = {x:0 , y:1} // Start the game
-    moveSound.play();
+    // moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp")
+            console.log("ArrowUp");
             inputDir.x= 0;
             inputDir.y= -1;
             break;
         case "ArrowDown":
-            console.log("ArrowDown")
+            console.log("ArrowDown");
             inputDir.x= 0;
             inputDir.y= 1;
             break;
         case "ArrowLeft":
-            console.log("ArrowLeft")
+            console.log("ArrowLeft");
             inputDir.x= -1;
             inputDir.y= 0;
             break;
         case "ArrowRight":
-            console.log("ArrowRight")
+            console.log("ArrowRight");
             inputDir.x= 1;
             inputDir.y= 0;
             break;
